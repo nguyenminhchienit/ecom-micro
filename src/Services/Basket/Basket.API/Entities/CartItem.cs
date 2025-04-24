ad98a7b52141758659ca1c01b3976e68d6d75c62
NguyenMinhChien <minhchien03112003@gmail.com>
2025-04-24 15:43:51 +0700

    add vilidation cart item

--- src/Services/Basket/Basket.API/Entities/CartItem.cs
+++ src/Services/Basket/Basket.API/Entities/CartItem.cs
@@ -1,13 +1,22 @@
-﻿namespace Basket.API.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Basket.API.Entities
 {
     public class CartItem
     {
+        [Required]
+        [Range(1, double.PositiveInfinity, ErrorMessage = "The feild {0} must be >= {1}")]
+
         public int Quantity { get; set; }
 
+        [Required]
+        [Range(0.1, double.PositiveInfinity, ErrorMessage = "The feild {0} must be >= {1}")]
         public decimal ProductPrice { get; set; }
 
+        [Required]
         public string ProductNo { get; set; }
 
+        [Required]
         public string ProductName { get; set; }
     }
 }
--- src/Services/Basket/Basket.API/obj/Debug/net6.0/Basket.API.AssemblyInfo.cs
+++ src/Services/Basket/Basket.API/obj/Debug/net6.0/Basket.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Basket.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d0cf174c3b4f78b951e94c1290ba27005c998c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dad23061c6a481cf2481001d410758766ea554aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Basket.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Basket.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
